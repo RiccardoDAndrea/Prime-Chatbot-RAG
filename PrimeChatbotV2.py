e4ecded2d6e1a0bbd1e588473d80cb3e2db83050
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-01-23 18:53:21 +0100

    adding vision models like llama3.2-vision as a list

--- PrimeChatbotV2.py
+++ PrimeChatbotV2.py
@@ -1,8 +1,20 @@
 import ollama
-response = ollama.chat(model='llama3.1', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
+
+ollamaModel_list = ["llama3.2-vision", "llama3.2-vision", "llama3.2"]
+
+user_input = "What is strange about this image?"
+
+with open('/run/media/riccardodandrea/Ricca_Data/ArmoredEye/Main_battle_tanks/Russia/T_90/T_90_40.jpg', 'rb') as file:
+  response = ollama.chat(
+    model= ollamaModel_list[1] ,
+    messages=[
+      {
+        'role': 'user',
+        'content': f'{user_input}',
+        'images': [file.read()],
+      },
+    ],
+  )
+
+print(response["model"] + " " + (response["created_at"]))
+print(response["message"]["content"])
