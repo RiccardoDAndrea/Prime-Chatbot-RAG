568fc927289e72d2706b6712f9a6de40e523166e
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-01-23 21:27:27 +0100

    create a dynamical list  comprhesion t o get all models from ollama

--- PrimeChatbotV2_LLM.py
+++ PrimeChatbotV2_LLM.py
@@ -1,11 +1,8 @@
-## Prime-Chatbot Version 2 
-### Die neue Version so die fahigkeit haben  mehre Model zu beinhalten
-### Dabei soll auch eine Optimerite Rag Kompatibilitat hinzugefugt werden
-
 import ollama
-
 from ollama import chat
 from ollama import ChatResponse
+
+
 ollamaModel_LLMlist = ["llama3.1", "llama3.2"]
 
 
@@ -25,6 +22,4 @@ def ollamaLLM_response(prompt:str, model:int):
     # or access fields directly from the response object
     return answer
 
-
-#print(response["model"] + " " + (response["created_at"]))
-#print(response["message"]["content"])
+ollamaModelvision_list = ["llava","llama3.2-vision"]
---
+++ PrimeChatbotV2_Vision.py
@@ -0,0 +1,32 @@
+import ollama
+from ollama import chat
+from ollama import ChatResponse
+
+
+
+models = ollama.list()["models"]
+ollamaModelvision_list = [model["model"] for model in models] # return all downloades ollama models
+
+user_input = "What is strange about this picture"
+
+def ollama_vision(prompt:str, model:int):
+    """
+    TODO: Desc
+    
+    """
+    with open('/run/media/riccardodandrea/Ricca_Data/ArmoredEye/Main_battle_tanks/Russia/T_90/T_90_40.jpg', 'rb') as file:
+        response = ollama.chat(
+            model= ollamaModelvision_list[model] ,
+            messages=[
+            {
+                'role': 'user',
+                'content': f'{user_input}',
+                'images': [file.read()],
+            },
+            ],
+        )
+    model = response['model']    
+    answer = response['message']['content']
+    return model, answer
+
+print(ollama_vision(prompt=user_input, model=3))
