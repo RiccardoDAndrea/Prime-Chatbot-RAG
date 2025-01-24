88e414a2418d205a0f70638835bd96645705404a
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-01-24 20:41:29 +0100

    adding a chot memory

--- PrimeChatbotV2_LLM.py
+++ PrimeChatbotV2_LLM.py
@@ -32,7 +32,7 @@ def ollamaLLM_response(prompt: str, model: int) -> str:
             {
                 "role": "user",
                 "content": f"""
-                You are a helpful Assistant that starts every answer with a funny Joke.
+                You are a helpful Assistant to every question.
                 {prompt}
                 """,
             },
@@ -42,7 +42,63 @@ def ollamaLLM_response(prompt: str, model: int) -> str:
     # Extract the response content
     answer = response.message['content']
     return answer
+#print(ollamaLLM_response(prompt="do you rember my name?", model=0))
 
 
-# Example usage
-print(ollamaLLM_response(prompt="come estas?", model=0))
+from ollama import chat
+
+messages = [
+    {
+        "role": "user",
+        "content": "Why is the sky blue",
+    },
+    {
+        "role": "assistant",
+        "content":"The sky is blue because of the way the Earths atmosphere scatters sunlight"
+    },
+    {
+        "role":"user",
+        "content": "what is the weather in Tokyo?",
+    },
+    {
+        "role": "assistant",
+        "content":"The weather in Tokoyo is typically warm and humid during the summer months, with temperatures often exceeding 30 30°C (86°F). The city experiences a rainy season from June to September, with heavy rainfall and occasional typhoons. Winter is mild, with temperatures rarely dropping below freezing. The city is known for its high-tech and vibrant culture, with many popular tourist attractions such as the Tokyo Tower, Senso-ji Temple, and the bustling Shibuya district.",
+    },
+    ]
+
+while True:
+  user_input = input('Chat with history: ')
+  response = chat(
+    'llama3.2',
+    messages=messages
+    + [
+      {'role': 'user', 'content': user_input},
+    ],
+  )
+
+  # Add the response to the messages to maintain the history
+  messages += [
+    {'role': 'user', 'content': user_input},
+    {'role': 'assistant', 'content': response.message.content},
+  ]
+  print(response.message.content + '\n') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
