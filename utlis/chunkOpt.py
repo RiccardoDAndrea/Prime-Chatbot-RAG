2a5d2d9465dfbcee22d274b12ab9ff43c4861dc0
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-02-16 17:38:27 +0100

    add PDF text extraction functionality and new document

--- utlis/chunkOpt.py
+++ utlis/chunkOpt.py
@@ -1,4 +1,28 @@
 # Optimizing the chucking process
 import os
+import pdfplumber
+
+def get_text(file_path: str) -> str:
+    """
+    
+    
+    """
+    with pdfplumber.open(file_path) as pdf:
+        text = ""
+        for page in pdf.pages:
+            page_text = page.extract_text(
+                x_tolerance=3, 
+                y_tolerance=3, 
+                layout=False, 
+                x_density=7.25, 
+                y_density=13, 
+                line_dir_render="ttb"
+            )
+            if page_text:  # Avoid adding None values
+                text += page_text + "\n"
+        return text
+
+print(get_text(file_path="PDF_docs/doc_0.pdf"))
+
 
 
