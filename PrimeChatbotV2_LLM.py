04c3705072f55b84026d4859d9b74093fbb633e0
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-02-21 11:08:50 +0100

    Refactor PrimeChatbot class to include model parameter and enhance method documentation

--- PrimeChatbotV2_LLM.py
+++ PrimeChatbotV2_LLM.py
@@ -9,17 +9,18 @@ from langchain_core.output_parsers import StrOutputParser
 import os
 
 class PrimeChatbot:
-    def __init__(self, file_path, chunk_size, chunk_overlap):
-        self.file_path = file_path
+    def __init__(self, file_path, model, chunk_size, chunk_overlap):
+        self.file_path=file_path
+        self.model = model
         self.chunk_size=chunk_size
-        self.chunk_overlap = chunk_overlap
+        self.chunk_overlap=chunk_overlap
 
 
     def pdfloader(self):
         """
         Extracts data from the PDF File as a String.
 
-        Parameters
+        Retrieves
         ----------
         self.file_path : str
             Path to the PDF-File.
@@ -27,71 +28,133 @@ class PrimeChatbot:
         Returns
         -------
         Strings form Dodcument.
+
         """
 
         loader = PyPDFLoader(self.file_path)
         docs = loader.load()
+
         return docs
 
 
     def chunkssplitter(self):
         """
         Splits the document into chunks.
+
+        Retrieves
+        ----------
+        self.chunk_size : int
+            - Number of chunks of text
+        self.chunk_overlap : int
+            - Number of characters of the previous text
+
+        Returns
+        -------
+        Strings form Dodcument.
+
         """
+        
         docs = self.pdfloader()       
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
             chunk_size=self.chunk_size, 
-            chunk_overlap=self.chunk_overlap
-        )
+            chunk_overlap=self.chunk_overlap)
+        
         doc_splits = text_splitter.split_documents(docs)
+
         return doc_splits
 
 
     def create_vectorstore(self):
         """
         Creates a vector store from a document by splitting it into chunks and embedding them.
+        
+        Retrives:
+        ---
+            self.chunkssplitter(): The split document into chunks and overlaps between the chunks
+
+        Returns:
+        ---
+        Collection of vectors 
+
         """
+
         doc_splits = self.chunkssplitter()
         vectorstore = SKLearnVectorStore.from_documents(
             documents=doc_splits,
             embedding=OllamaEmbeddings(model="llama3.2:latest")
         )
+
         return vectorstore
 
 
     def Retriever(self, k_int=5):
         """
         Retrieves a specified number of relevant documents.
+        Retrives:
+        ---
+            self.create_vectorstore(): Creates a vector store from a document by 
+                                       splitting it into chunks and embedding them.
+
+        Parameter:
+            k_int (int): The number of top-scoring documents to retrieve. Defaults to 5.
+
+        Returns:
+            A retriever object that can be used to search for documents based on 
+            their similarity score.
         """
+
         vectorstore = self.create_vectorstore()
         retriever = vectorstore.as_retriever(k=k_int,
-                                            search_type="similarity_score_threshold",
-                                            search_kwargs={'score_threshold': 0.8})
+                                            search_type="similarity",
+                                            search_kwargs={"k": k_int}
+                                            )
         return retriever
 
 
     def promptTemplate(self):
         """
-        Creates the prompt template for the language model.
+        Creates the prompt Instruction template for the language model.
+
+        Retrives:
+            Question: str
+                Query from the user
+
+        Returns:
+            Intruction for the LLM-Model
         """
+
         prompt = PromptTemplate(
-            template="""<s>[INST] <<SYS>>You are a friendly assistant called “Prime Chatbot” who 
-            answers questions about the added source. If you do not have an answer, say that you 
-            cannot answer the question satisfactorily. <</SYS>>
+            template="""<s>[INST] <<SYS>>
+            You are an expert AI assistant. Answer the given question based on the provided documents. 
+            If the documents do not contain the answer, say 'I don't know'. Do not summarize.
+            <</SYS>>
             Question: {question}
             Documents: {documents}
-            [/INST]""",
+            Answer:""",
             input_variables=["question", "documents"],
-        )
+            )
         return prompt
 
 
     def llm(self, model):
         """
         Initializes the language model.
+
+        Retrives:
+        ---
+            self.model(): Creates a vector store from a document by 
+                          splitting it into chunks and embedding them.
+
+        Parameter:
+            k_int (int): The number of top-scoring documents to retrieve. Defaults to 5.
+
+        Returns:
+            A retriever object that can be used to search for documents based on 
+            their similarity score.
         """
+        
         llm = ChatOllama(
-            model=model,
+            model=self.model,
             temperature=0.5
         )
         return llm
@@ -99,11 +162,25 @@ class PrimeChatbot:
 
     def ragchain(self):
         """
+
+        
         Creates the RAG (Retrieval-Augmented Generation) chain.
+        
+        Retrives:
+        ---
+            self.promptTemplate(): 
+                Creates the prompt Instruction template for the language model.
+            self.llm(): 
+                Initializes the language model.
+        Returns
+        ---
+        A RagChain object that combines an Instruction template and a language model.
+        The RagChain object contains a sequence of operations, starting with the creation of a prompt instruction template,
+        followed by the initialization of a language model. This chain can be used for text generation tasks.
+
         """
-        prompt = self.promptTemplate()
-        llm = self.llm(model='llama3.2:latest')
-        rag_chain = prompt | llm 
+        rag_chain = self.promptTemplate() | self.llm(self.model) | StrOutputParser()
+
         return rag_chain
 
 
@@ -122,12 +199,16 @@ class PrimeChatbot:
 
 
 # Initialize the RAG application
-PrimeChatbot = PrimeChatbot(file_path='PDF_docs/doc_0.pdf', chunk_size=500, chunk_overlap=300)
+PrimeChatbot = PrimeChatbot(file_path='PDF_docs/doc_0.pdf', 
+                            model= "llama3.1", 
+                            chunk_size=500, 
+                            chunk_overlap=300)
+
+
 docs = PrimeChatbot.Retriever()
-print(docs)
+#print(docs)
 
-
-question = "Who are the authors on the paper Two Hundred Years of Cancer Research ?"
+question = "Can you give me some sources that have been added to the document?"
 answer = PrimeChatbot.initaliseChatbot(question)
 print("Question:", question)
 print("Answer:", answer)
